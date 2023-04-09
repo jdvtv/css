c2accf3a2d7c497db26ac3061daa25404a46d9e6
jdvtv <84622872+jdvtv@users.noreply.github.com>
2023-04-09 17:32:43 -0300

    Test.js

--- Test.js
+++ Test.js
@@ -33,9 +33,9 @@ var SearchExample = React.createClass({
         }
 
         return <div>
-                    <input id="searchjdv" type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Digite uma palavra" />
+                    <input id="searchjdv" type="search" value={this.state.searchString} onChange={this.handleChange} placeholder="Digite uma palavra" />
 
-                    <td> 
+                    <td class"jdvtab"> 
 
                         { libraries.map(function(l){
                             return <th><a href={l.url}><img src={l.src}/><h2>{l.name}</h2><p>{l.description}</p></a></th>
