989064cc8754c826fb9fb095541088c24229ea2c
jdvtv <84622872+jdvtv@users.noreply.github.com>
2023-04-10 09:52:44 -0300

    Test.js

--- Test.js
+++ Test.js
@@ -33,7 +33,7 @@ var SearchExample = React.createClass({
         }
 
         return <div>
-                    <textarea type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Digite uma palavra"></textarea>
+                    <input type="text" value={this.state.searchString} onChange={this.handleChange} placeholder="Digite uma palavra" />
 
                     <ta> 
 
