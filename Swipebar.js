98fa24005d5014ef344cab35e2a5d3cd5fd31b9f
jdvtv <84622872+jdvtv@users.noreply.github.com>
2023-04-18 07:28:18 -0300

    Swipebar.js

---
+++ Swipebar.js
@@ -0,0 +1,40 @@
+var items = document.querySelectorAll('.overlay');
+
+items.forEach(item => {
+  item.addEventListener('touchstart', handleTouchStart, false);
+  item.addEventListener('touchmove', handleTouchMove, false);
+});
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches || evt.originalEvent.touches;
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+  elm = firstTouch.target;
+};
+
+function handleTouchMove(evt) {
+  if ( ! xDown || ! yDown ) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+
+  if ( xDiff > 0 ) {
+    elm.style.top = '0px';
+  } else {
+    elm.style.top = '-190px';
+  }
+
+  xDown = null;
+  yDown = null;
+};
