e63e0e302b1db50c1fb3b556a8ba7b1324656684
jdvtv <84622872+jdvtv@users.noreply.github.com>
2023-07-07 12:25:15 -0300

    Swipebar.js

--- Swipebar.js
+++ Swipebar.js
@@ -30,9 +30,9 @@ function handleTouchMove(evt) {
   var xDiff = xDown - xUp;
 
   if ( xDiff > 0 ) {
-    elm.style.top = '-648px;
-  } else {
     elm.style.top = '0';
+  } else {
+    elm.style.top = '-648px';
   }
 
   xDown = null;
