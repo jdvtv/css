5ad11f7237ea2726d2d1f47198399862f10fcc83
jdvtv <84622872+jdvtv@users.noreply.github.com>
2023-11-08 20:24:02 -0300

    Update BibiliotecaOS.js

--- BibiliotecaOS.js
+++ BibiliotecaOS.js
@@ -1,3 +1,1 @@
-
-
-<script src="https://cdn.jsdelivr.net/gh/jdvtv/css@d61bbc0802e1222faf83a844bb5db529c1ddd4aa/volumecontrol.js"></script>
+https://cdn.jsdelivr.net/gh/jdvtv/css@d61bbc0802e1222faf83a844bb5db529c1ddd4aa/volumecontrol.js
