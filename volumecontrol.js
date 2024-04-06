fa7cdf4cf3051c5daf1eaef62999062226b54e03
jdvtv <84622872+jdvtv@users.noreply.github.com>
2024-04-06 18:09:02 -0300

    Update volumecontrol.js

--- volumecontrol.js
+++ volumecontrol.js
@@ -5,6 +5,6 @@ $('#screen').on('swipeleft',function(){
 $('#screen').on('swiperight',function(){
      $(".white-select-left").css("animation","explosion 0.7s ease-in").css("animation-fill-mode","forwards");
 });
-function volumeup(){ document.getElementById("container-volume").style.display = "block"; setTimeout(function(){ document.getElementById("container-volume").style.display = "none"; }, 4000); document.getElementById("control").stepUp(1); }
+function volumeup(){ $("#container-volume").show(); setTimeout(function(){ $("#container-volume").hide(); }, 4000); document.getElementById("control").stepUp(1); }
 
-function volumedown(){ document.getElementById("container-volume").style.display = "block"; setTimeout(function(){ document.getElementById("container-volume").style.display = "none"; }, 4000); document.getElementById("control").stepDown(1); }
+function volumedown(){ $("#container-volume").show(); setTimeout(function(){ $("#container-volume").hide(); }, 4000);document.getElementById("control").stepDown(1); }
